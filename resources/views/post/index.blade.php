c0447330c952d1231121ecd3f6c8270dee6f483f
KeitaShimura <keitashimura2023@gmail.com>
2023-02-16 06:46:42 +0900

    自分の投稿のみに編集削除ボタンを表示

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -18,11 +18,13 @@
         <div class="card-body">
           <h5 class="card-title"><a href="{{ route('post.show', $post->id) }}">{{$post->title}}</a></h5>
           <p class="card-text">{{$post->comment}}</p>
+          @if($post->user_id == Auth::user()->id)
           <a href="{{ route('post.edit', $post->id) }}">編集</a>
           <form method="POST" action="{{ route('post.destroy', $post->id) }}">
             @csrf
             @method('DELETE')
             <button class="btn btn-danger">削除</button></td>
+            @endif
         </form>
         </div>
     @endforeach
